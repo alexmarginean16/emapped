7ff7fe28b9125f27f8423bc24379fd433d8dda05
Paul <paldeac2@gmail.com>
2021-04-10 18:14:33 +0100

    flaskadd

--- app.py
+++ app.py
@@ -2,20 +2,23 @@ from functools import wraps
 import sys
 import os
 import io
-from flask import Flask, render_template, redirect, request, url_for, session, abort, \
+from flask import Flask, render_template, flash, redirect, request, url_for, session, abort, \
     send_from_directory
 #coming from pyrebase4
 from werkzeug.utils import secure_filename
 import pyrebase
 import imghdr
-from google.cloud import vision
-
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "emapped-8b4be6305e9a.json"
-
-client = vision.ImageAnnotatorClient()
 
 #firebase config
 config = {
+  "apiKey": "AIzaSyAKf4sNxkZ3NGqd2pJYrHEN0SJ-abfkLlI",
+  "authDomain": "emapped.firebaseapp.com",
+  "databaseURL": "https://emapped-default-rtdb.firebaseio.com",
+  "projectId": "emapped",
+  "storageBucket": "emapped.appspot.com",
+  "messagingSenderId": "492726205148",
+  "appId": "1:492726205148:web:bba13e995c1ee2cc661219",
+  "measurementId": "G-YV4E96ZP18"
 
 }
 
@@ -24,8 +27,7 @@ firebase = pyrebase.initialize_app(config)
 #auth instance
 auth = firebase.auth()
 #real time database instance
-db = firebase.database();
-
+db = firebase.database()
 
 #new instance of Flask
 app = Flask(__name__)
@@ -70,7 +72,7 @@ def signup():
       password = request.form["password"]
       try:
         #create the user
-        auth.create_user_with_email_and_password(email, password);
+        auth.create_user_with_email_and_password(email, password)
         #login the user right away
         user = auth.sign_in_with_email_and_password(email, password)
         #session
@@ -78,6 +80,7 @@ def signup():
         user_email = email
         session['usr'] = user_id
         session["email"] = user_email
+        os.mkdir('photos/' + user_email)
         return redirect("/")
       except:
         return render_template("login.html", message="The email is already taken, try another one, please" )
@@ -117,7 +120,7 @@ def logout():
     #session['usr'] = ""
     #session["email"] = ""
     session.clear()
-    return redirect("/");
+    return redirect("/")
 
 def validate_image(stream):
     header = stream.read(512)  # 512 bytes should be enough for a header check
@@ -127,83 +130,36 @@ def validate_image(stream):
         return None
     return '.' + (format if format != 'jpeg' else 'jpg')
 
-#create form
+#upload form
 @app.route("/upload", methods=["GET", "POST"])
 @isAuthenticated
 def upload():
   if request.method == "POST":
-    if os.path.isdir('photos/' + session["email"]):
-      app.config['UPLOAD_PATH'] = 'photos/' + session["email"]
-      #get the request data
-      uploaded_file = request.files["upload"]
-      filename = secure_filename(uploaded_file.filename)
-      path = "photos/" + session["email"] + "/" + filename
-      # file_name = os.path.abspath('/photos/' + session["email"] + "/" + uploaded_file.filename)
-      with io.open(path, 'rb') as image_file:
-        content = image_file.read();
+    app.config['UPLOAD_PATH'] = 'photos/' + session["email"]
+    uploaded_file = request.files["file"]
 
-      image = vision.Image(content=content)
-
-      # Performs label detection on the image file
-      response = client.label_detection(image=image)
-      labels = response.label_annotations
-
-      tags = []
-      for label in labels:
-          tags.append(str(label.description))
-
-      print(tags)
-
-      # Performs face detection on the image file
-      response = client.face_detection(image=image)
-      faces = response.face_annotations
-
-      likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE', 'LIKELY', 'VERY_LIKELY')
-
-      moods = []
-
-      for face in faces:
-        mood = []
-        mood.append('Joy')
-        mood.append(likelihood_name.index(likelihood_name[face.joy_likelihood]))
-        moods.append(mood)
-
-        mood = []
-        mood.append('Sorrow')
-        mood.append(likelihood_name.index(likelihood_name[face.sorrow_likelihood]))
-        moods.append(mood)
-
-        mood = []
-        mood.append('Anger')
-        mood.append(likelihood_name.index(likelihood_name[face.anger_likelihood]))
-        moods.append(mood)
-
-        mood = []
-        mood.append('Surprise')
-        mood.append(likelihood_name.index(likelihood_name[face.surprise_likelihood]))
-        moods.append(mood)
-
-     # filename = secure_filename(uploaded_file.filename)
-      if filename != '':
-          file_ext = os.path.splitext(filename)[1]
-          if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
-                  file_ext != validate_image(uploaded_file.stream):
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+      file_ext = os.path.splitext(filename)[1]
+      if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
+              file_ext != validate_image(uploaded_file.stream):
               abort(400)
-          uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-    else:
-      os.makedirs('photos/' + session["email"])
+      uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+  return render_template("upload.html")
 
-  return render_template("upload.html", email = session["email"])
-
-@app.route("/post")
+@app.route("/upload/<filename>")
 @isAuthenticated
-def post():
-    pics = os.listdir('photos/' + session["email"])
-    for photo in pics:
-      image = Image.open('photos/' + session["email"]+ "/" + photo)
-      for (tag,value) in image._getexif().iteritems():
-        print (TAGS.get(tag), value)
-    return render_template("post.html", pics = pics)
+def send_images(filename):
+  return send_from_directory("photos/"+session["email"], filename)
+
+
+@app.route("/posts")
+@isAuthenticated
+def get_photos():
+  image_names = os.listdir('photos/' + session["email"])
+  print(image_names)
+  return render_template("post.html", image_names=image_names)
+
 
 
 #run the main script
